2c524275b1fa4abbf45bbaae3d19441f6eec5a16
Ming Sun <msun@kinghanhong.com>
2012-08-02 16:27:28 +0800

    添加用于编译eCardFree和eCardPro的target

---
+++ eCardPro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eCardPro
+//
+//  Created by 孙铭 on 8/2/12.
+//  Copyright (c) 2012 KingHanHong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KHHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KHHAppDelegate class]));
+    }
+}
