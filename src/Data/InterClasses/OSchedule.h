d72cf80f99e7148e328255896e04e938c3e8f9e9
孙铭 <msun@kinghanhong.com>
2012-10-23 16:32:53 +0800

    新建拜访计划

---
+++ src/Data/InterClasses/OSchedule.h
@@ -0,0 +1,13 @@
+//
+//  OSchedule.h
+//  CardBook
+//
+//  Created by Sun Ming on 12-10-23.
+//  Copyright (c) 2012年 Kinghanhong. All rights reserved.
+//
+
+#import "SMObject.h"
+
+@interface OSchedule : SMObject
+
+@end
