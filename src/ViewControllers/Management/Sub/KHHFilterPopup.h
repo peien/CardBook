866639c11c7acb84ac70a9783154783248ad2b91
CJK <xiaofei779985@126.com>
2012-12-20 13:51:34 +0800

    alertView

--- src/ViewControllers/Management/Sub/KHHFilterPopup.h
+++ src/ViewControllers/Management/Sub/KHHFilterPopup.h
@@ -8,6 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
-@interface KHHFilterPopup : NSObject
+@protocol KHHFilterPopupDelegate <NSObject>
+@optional
+- (void)selectInAlert:(NSString *)index;
+@end
+
+@interface KHHFilterPopup : NSObject <UITableViewDelegate, UITableViewDataSource> {
+    
+    UITableView	*popUpBoxTableView;
+    UIAlertView *alert;
+    NSArray *dataArr;
+    int seleIndex;
+}
+@property (nonatomic,strong) id<KHHFilterPopupDelegate> delegate;
++ (KHHFilterPopup *)shareUtil;
+
+- (void)showPopUp:(NSArray *)array index:(int)index delegate:(id<KHHFilterPopupDelegate>)delegate;
+- (void)showPopUp:(NSArray *)array index:(int)index Title:(NSString *)title delegate:(id<KHHFilterPopupDelegate>)delegate;
 
 @end
