1b790129358c89ac3ea2210fbe8e6dc7b52d487a
CJK <xiaofei779985@126.com>
2013-01-05 10:35:18 +0800

    for new

---
+++ src/Util/KHHDateUtil.h
@@ -0,0 +1,13 @@
+//
+//  KHHDateUtil.h
+//  CardBook
+//
+//  Created by CJK on 13-1-4.
+//  Copyright (c) 2013年 Kinghanhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KHHDateUtil : NSObject
+
+@end
---
+++ src/Util/KHHDateUtil.m
@@ -0,0 +1,13 @@
+//
+//  KHHDateUtil.m
+//  CardBook
+//
+//  Created by CJK on 13-1-4.
+//  Copyright (c) 2013年 Kinghanhong. All rights reserved.
+//
+
+#import "KHHDateUtil.h"
+
+@implementation KHHDateUtil
+
+@end
